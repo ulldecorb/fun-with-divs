f7a08a38db3ae425312b8c5ae68302dcf735a746
ulldecorb <ausias.bertran@gmail.com>
2022-02-06 22:12:14 +0100

    Create counter component

--- fun-with-divs/src/App.jsx
+++ fun-with-divs/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Divs } from './components/Divs';
+import { Counter } from './components/Counter';
 import './App.css';
 
 function App() {
+  // const divRigthRotationDegree = 0;
+  // const divLeftRotationDegree = 0;
+
   return (
     <div className="App">
       <Divs />
+      <Counter />
     </div>
   );
 }
