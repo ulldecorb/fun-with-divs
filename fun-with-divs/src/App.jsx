dde258240f6656429bdfc32c5caa7c559ae10461
ulldecorb <ausias.bertran@gmail.com>
2022-02-06 02:20:44 +0100

    Create div component

--- fun-with-divs/src/App.jsx
+++ fun-with-divs/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Divs } from './components/Divs';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      hola
+      <Divs />
     </div>
   );
 }
