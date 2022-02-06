b6327e32071d5788104b25a891520573e89a62b7
ulldecorb <ausias.bertran@gmail.com>
2022-02-06 01:31:18 +0100

    Add eslint linter Add prop-types library Add react-router library Add react-router-dom library

--- fun-with-divs/src/App.jsx
+++ fun-with-divs/src/App.jsx
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
+      hola
     </div>
   );
 }
