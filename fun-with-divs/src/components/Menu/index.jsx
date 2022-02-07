2576c812ab9ad86a45406da517462081f7741982
ulldecorb <ausias.bertran@gmail.com>
2022-02-07 23:57:08 +0100

    Add color form on menu component

--- fun-with-divs/src/components/Menu/index.jsx
+++ fun-with-divs/src/components/Menu/index.jsx
@@ -2,11 +2,24 @@ import React from 'react';
 import './menu.css';
 
 export const Menu = function Menu() {
+  const handleColor = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <section className="menu">
       <div className="menu__box">
         <input type="range" name="rotation-speed" id="rotation-speed" max="10" min="2" />
-        <input type="color" name="divs-color" id="divs-color" />
+        <form onSubmit={handleColor}>
+          <label>
+            Select style:
+            <input type="radio" name="style" id="red" />
+            <input type="radio" name="style" id="noir" />
+            <input type="radio" name="style" id="gum" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        {/* <input type="color" name="divs-color" id="divs-color" /> */}
         <button type="button">RESET</button>
       </div>
     </section>
